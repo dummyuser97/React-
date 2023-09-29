5a6046ac34e6b511fd0d20d159258bdb0debf458
dummyuser97 <dummyemail20051997@gmial.com>
2023-09-29 14:58:06 +0530

    Commit after tutorial 4 - basic understanding

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+let name = "Harry";
+function App() {
+  return (
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <h1> Hello {name}</h1>
+    <div className="container">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae earum maiores alias quos quas laudantium similique, architecto fugit?
+     Optio fugit rem illo similique quidem voluptatem sint voluptates temporibus incidunt iure consectetur, sequi et? Ab, atque!</div>
+    <div className="blank">Lovely</div>
+    </>
+    );
+}
+
+export default App;
